c27bb0f3ed3cfc4689e7f90a8d4b2b191d666cda
daominhkhan20 <khanhc1k36@gmail.com>
2022-01-01 20:46:38 +0700

    update

--- Model/ML.py
+++ Model/ML.py
@@ -1,26 +1,26 @@
 from classification import Model
 import torch
-import os
-import random
 import pandas as pd
 from pandas import DataFrame
 
 import torch
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import Ridge
-from sklearn.metrics import make_scorer
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 import argparse
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name', type=str, default='ridge')
-
+parser.add_argument('--is_train_time', default=True, type=lambda x: str(x).lower() == 'true')
+parser.add_argument('--path_model', type=str, default='decision_tree1.pkl', help='Path to pretrained model')
+parser.add_argument('--path_map_feature', type=str, default='map_decision_tree1.pkl')
 arg = parser.parse_args()
 
 
@@ -39,32 +39,51 @@ data_test = pd.read_csv('../Preprocess/test_data.csv')
 data_train_des = make_df_description(data_train, '../Description/train_des.pth')
 data_test_des = make_df_description(data_test, '../Description/test_des.pth')
 
-if __name__=='__main__':
+if __name__ == '__main__':
+    if arg.is_train_time:
+        if arg.model_name == 'ridge':
+            ml_algorithm = Pipeline(
+                steps=[
+                    ("scaler", MinMaxScaler()),
+                    ('model', Ridge(alpha=0.2))
+                ])
+            parameters = [ {'model__alpha': [0,0.2,0.4,0.8] } ]
 
-    if arg.model_name == 'ridge':
-        ml_algorithm = Pipeline(
-            steps=[
+        elif arg.model_name == 'knn':
+            ml_algorithm = Pipeline(steps=[
                 ("scaler", MinMaxScaler()),
-                ('model', Ridge(alpha=0.2))
+                ('model', KNeighborsRegressor(n_neighbors=5))
             ])
+            parameters = [{'model__n_neighbors': [5, 10, 20, 30, 40]} ]
 
-    elif arg.model_name == 'knn':
-        ml_algorithm = Pipeline(steps=[
-            ("scaler", MinMaxScaler()),
-            ('model', KNeighborsRegressor(n_neighbors=5))
-        ])
+        elif arg.model_name == 'decision_tree':
+            ml_algorithm = Pipeline(steps=[
+                ("scaler", MinMaxScaler()),
+                ('model', DecisionTreeRegressor(random_state=0))
+            ])
+            parameters = [{
+                'model__max_depth': [10, 20, 30, 50],
+                'model__min_samples_split': [1, 3, 10],
+                'model__min_samples_leaf': [2, 5, 10],
+                'model__max_features': ['auto', 'sqrt']
+            }]
 
-    elif arg.model_name == 'decision_tree':
-        ml_algorithm = Pipeline(steps=[
-            ("scaler", MinMaxScaler()),
-            ('model', DecisionTreeRegressor(random_state=0))
-        ])
+        model = Model(ml_algorithm, data_train, data_test, parameters, arg.model_name)
+        mse_loss, r2Score = model.build()
+        model.grid_search()
+        model.save_model(f'{arg.model_name}.pkl', f'map_{arg.model_name}.pkl')
+        print(f"Mean squared score: {mse_loss}\nR2 score:{r2Score}\n\n\n")
 
-    model = Model(ml_algorithm, data_train, data_test)
-    model1 = Model(ml_algorithm, data_train_des, data_test_des)
-    mse_loss, r2Score = model.build()
-    print(f"Mean squared score: {mse_loss}\nR2 score:{r2Score}")
-    mse_loss1, r2Score1 = model1.build()
-    print(f"Mean squared score: {mse_loss1}\nR2 score:{r2Score1}")
+        model1 = Model(ml_algorithm, data_train_des, data_test_des, parameters, arg.model_name)
+        mse_loss1, r2Score1 = model1.build()
+        model1.grid_search()
+        model1.save_model(f'{arg.model_name}_des.pkl', f'map_{arg.model_name}_des.pkl')
+        print(f"Mean squared score: {mse_loss1}\nR2 score:{r2Score1}")
 
+    else:
+        with open(arg.path_model, 'rb') as file:
+            ml_algorithm = pickle.load(file)
 
+        model = Model(ml_algorithm, is_train_time=False, path_feature_map=arg.path_map_feature)
+        predict = model.predict_sample(data_test.head(1))
+        print(predict)
--- Model/classification.py
+++ Model/classification.py
@@ -1,20 +1,50 @@
 from pandas import DataFrame
 import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, make_scorer
 import pickle
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+import os
+
+
+def score_evaluate_function(actual, prediction):
+    return mean_squared_error(actual, prediction)
+
+
+grid_scorer = make_scorer(score_evaluate_function, greater_is_better=True)
 
 
 class Model:
-    def __init__(self, model, data_train: DataFrame, data_test: DataFrame):
+    def __init__(self, model,
+                 data_train: DataFrame = None,
+                 data_test: DataFrame = None,
+                 parameters: list = None,
+                 model_name: str = None,
+                 is_train_time: bool = True,
+                 path_feature_map: str = None
+                 ):
         self.model = model
         self.list_feature = ['origin', 'status', 'car_mileage', 'car_color', 'car_seats', 'gear', 'car_year',
                              'car_price', 'style', 'model', 'fuel', 'brand_name', 'region_name', 'month', 'year']
-        self.data_train = self.make_df(data_train)
-        self.data_test = self.make_df(data_test)
-        self.map_features = {}
-        self.convert_feature()
-        self.x_train, self.y_train = self.get_data(self.data_train)
-        self.x_test, self.y_test = self.get_data(self.data_test)
+        if is_train_time:
+            self.data_train = self.make_df(data_train)
+            self.data_test = self.make_df(data_test)
+            self.map_features = {}
+            self.convert_feature()
+            self.model_name = model_name
+            self.parameters = parameters
+            self.x_train, self.y_train = self.get_data(self.data_train)
+            self.x_test, self.y_test = self.get_data(self.data_test)
+        else:
+            self.list_feature.remove('car_price')
+            with open(path_feature_map, 'rb') as file:
+                self.map_features = pickle.load(file)
+
+    def grid_search(self):
+        grid_search = RandomizedSearchCV(self.model, param_distributions=self.parameters, scoring=grid_scorer,
+                                         cv=5)
+        grid_search.fit(self.x_train, self.y_train)
+        print(grid_search.best_params_)
 
     def make_df(self, df: DataFrame):
         df = df[self.list_feature]
@@ -23,15 +53,16 @@ class Model:
         return df.reset_index(drop=True)
 
     def convert_feature(self):
+        self.map_features['mode'] = []
         for column_name in self.list_feature:
             if self.data_train[column_name].dtype == 'object':
                 list_categorical = set(self.data_train[column_name])
                 map_f = {category: i for i, category in enumerate(list_categorical)}
                 self.data_train[column_name] = self.data_train[column_name].map(map_f)
                 self.data_test[column_name] = self.data_test[column_name].map(map_f)
-
+                mode_value = self.data_train[column_name].mode().values[0]
+                self.map_features['mode'].append(mode_value)
                 if self.data_test[column_name].isnull().values.any():
-                    mode_value = self.data_train[column_name].mode().values[0]
                     self.data_test[column_name].fillna(mode_value, inplace=True)
 
                 self.map_features[column_name] = map_f
@@ -41,13 +72,29 @@ class Model:
         df_temp = df.drop(['car_price'], axis=1)
         return df_temp.values, y
 
-    def save_model(self, model_name: str):
-        with open('map_features.pkl', 'wb') as file:
+    def predict_sample(self, df: DataFrame):
+        df = df[self.list_feature]
+        categorical_name = list(self.map_features.keys())
+        categorical_name.remove('mode')
+        for idx, column_name in enumerate(categorical_name):
+            df[column_name] = df[column_name].map(self.map_features[column_name])
+            if df[column_name].isnull().values.any():
+                df[column_name].fillna(self.map_features['mode'][idx], inplace=True)
+        x = df.values
+        predict_price = self.model.predict(x)
+        return np.exp(predict_price) * 1e6
+
+    def save_model(self, model_name: str, map_feature_name: str):
+        path = f'Pickle/{self.model_name}/'
+        if os.path.exists(path) is False:
+            os.makedirs(path, exist_ok=True)
+
+        with open(path + map_feature_name, 'wb') as file:
             pickle.dump(self.map_features, file, protocol=pickle.HIGHEST_PROTOCOL)
 
         print("Save map feature done")
 
-        with open(model_name, 'wb') as file:
+        with open(path + model_name, 'wb') as file:
             pickle.dump(self.model, file, pickle.HIGHEST_PROTOCOL)
 
         print("Save model done")
